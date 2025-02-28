8febafb283a789eec4d38c97418cb7f2f004f5e9
Roro4112 <144170397+Roro4112@users.noreply.github.com>
2025-02-28 11:47:57 -0700

    Update RangeTest.java with shift and expand tests

--- JFreeChart/src/org/jfree/data/test/RangeTest.java
+++ JFreeChart/src/org/jfree/data/test/RangeTest.java
@@ -164,6 +164,130 @@ public class RangeTest {
     public void testConstrainAboveUpperBound() {
         assertEquals("A value above the upper bound should return the upper bound", 1, exampleRange.constrain(2), 0.000000001d);
     }
+    //Shift() section
+     @Test
+    public void testPositiveDeltaValueWithNoZeroCrossingAndZeroCrossingFalseForShift() {
+    	expectedRange = new Range(0,2);
+    	assertEquals("After shifting by 1 with no zero crossing the range -1,1 should become 0,2",expectedRange,Range.shift(exampleRange,1));
+    }
+    
+    @Test
+    public void testNegativeDeltaValueWithNoZeroCrossingAndZeroCrossingFalseForShift() {
+    	expectedRange = new Range(-2,0);
+    	assertEquals("After shifting by -1 with no zero crossing the range -1,1 should become -2,0",expectedRange,Range.shift(exampleRange, -1));
+    }
+    
+    @Test
+    public void testPositiveDeltaValueWithNoZeroCrossingAndZeroCrossingTrueForShift() {
+    	expectedRange = new Range(0,2);
+    	assertEquals("After shifting by 1 with zero crossing the range -1,1 should become 0,2",expectedRange,Range.shift(exampleRange,1,true));
+    }
+    
+    @Test
+    public void testNegativeDeltaValueWithNoZeroCrossingAndZeroCrossingTrueForShift() {
+    	expectedRange = new Range(-2,0);
+    	assertEquals("After shifting by -1 with zero crossing the range -1,1 should become -2,0",expectedRange,Range.shift(exampleRange, -1,true));
+    }
+    
+    @Test
+    public void testNegativeDeltaValueWithZeroCrossingFalseForShift() {
+    	expectedRange = new Range(-3,0);
+    	assertEquals("After shifting by -2 with no zero crossing the range -1,1 should become -3,0",expectedRange,Range.shift(exampleRange, -2));
+    }
+    
+    @Test
+    public void testPositiveDeltaValueWithZeroCrossingFalseForShift() {
+    	expectedRange = new Range(0,3);
+    	assertEquals("After shifting by 2 with no zero crossing the range -1,1 should become 0,3",expectedRange,Range.shift(exampleRange, 2));
+    }
+    
+    @Test
+    public void testPositiveDeltaValueWithZeroCrossingTrueForShift() {
+    	expectedRange = new Range(1,3);
+    	assertEquals("After shifting by 2 with zero crossing the range -1,1 should become 1,3",expectedRange,Range.shift(exampleRange, 2, true));
+    }
+    
+    @Test
+    public void testNegativeDeltaValueWithZeroCrossingTrueForShift() {
+    	expectedRange = new Range(-3,-1);
+    	assertEquals("After shifting by -2 with zero crossing the range -1,1 should become -3,-1",expectedRange,Range.shift(exampleRange, -2, true));
+    }
+
+    @Test
+    public void testZeroDeltaValueWithZeroCrossingFalseForShift() {
+    	expectedRange = new Range(-1,1);
+    	assertEquals("After shiftingg by 0 with no zero crossing the range -1,1 should remain 1,1", expectedRange,Range.shift(exampleRange,0));
+    }
+    
+    @Test
+    public void testZeroDeltaValueWithZeroCrossingTrueForShift() {
+    	expectedRange = new Range(-1,1);
+    	assertEquals("After shiftingg by 0 with zero crossing the range -1,1 should remain 1,1", expectedRange,Range.shift(exampleRange,0));
+    }
+    
+    @Test
+    public void testExceptionWhenNullRangePassedForShift() {
+    	try {
+    		Range.shift(null, 1);
+    	}catch(InvalidParameterException e) {
+    		
+    	}catch(Exception e){
+    		fail("When passed a null shift should throw a InvalidParameterError but instead throws:" + e.getMessage());
+    	}
+    }
+
+    //Expand Section()
+    @Test
+    public void testLowerAndUpperBoundWithPercentage() {
+    	expectedRange = new Range(-2,2);
+    	assertEquals("After expanding the range -1,1 by 0.5,0.5 the returned range should be -2,2",expectedRange,Range.expand(exampleRange, 0.5, 0.5));
+    }
+    @Test
+    public void testZeroLowerMarginForExpand() {
+    	expectedRange = new Range(-1,2);
+    	assertEquals("After expanding the range -1,1 by 0,0.5 the returned range should be -1,2",expectedRange,Range.expand(exampleRange, 0, 0.5));
+    }
+    
+    @Test
+    public void testZeroUpperMarginForExpand() {
+    	expectedRange = new Range(-2,1);
+    	assertEquals("After expanding the range -1,1 by 0.5,0 the returned range should be -2,1",expectedRange,Range.expand(exampleRange, 0.5, 0));
+    }
+    
+    @Test
+    public void testZeroLowerMarginAndUpperMarginForExpand() {
+    	expectedRange = new Range(-1,1);
+    	assertEquals("After expanding the range -1,1 by 0,0 the returned range should be -1,1",expectedRange,Range.expand(exampleRange, 0, 0));
+    }
+    
+    @Test
+    public void testLowerMarginGreaterThanOne() {
+    	expectedRange = new Range(-5,2);
+    	assertEquals("After expanding the range -1,1 by 2,0.5 the returned range should be -5,2",expectedRange,Range.expand(exampleRange, 2, 0.5));
+    }
+    
+    @Test
+    public void testUpperMarginGreaterThanOne() {
+    	expectedRange = new Range(-2,5);
+    	assertEquals("After expanding the range -1,1 by 0.5,2 the returned range should be -2,5",expectedRange,Range.expand(exampleRange, 0.5, 2));
+    }
+    
+    @Test
+    public void testLowerAndUpperMarginGreaterThanOne() {
+    	expectedRange = new Range(-5,5);
+    	assertEquals("After expanding the range -1,1 by 2,2 the returned range should be -5,5",expectedRange,Range.expand(exampleRange, 2, 2));
+    }
+    
+    @Test
+    public void testExceptionWhenNullRangePassedForExpand() {
+    	try {
+    		Range.expand(null, 1,1);
+    	}catch(InvalidParameterException e) {
+    		
+    	}catch(Exception e){
+    		fail("When passed a null shift should throw a InvalidParameterError but instead throws:" + e.getMessage());
+    	}
+    }
 
     @After
     public void tearDown() throws Exception {
